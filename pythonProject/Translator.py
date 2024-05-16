fb04c787943855fe0f4a865ca08ae17949d46d18
DSquire9 <dss9593@rit.edu>
2024-05-16 11:18:25 -0400

    Improved translator

--- pythonProject/Translator.py
+++ pythonProject/Translator.py
@@ -6,13 +6,22 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        #find the index of the letter in the alphabet
-        index = alphabet.index(letter)
-        #shift it 13 positions
-        translation = translation + alphabet[(index + 13) % 26]
+        # find the index of the letter in the alphabet
+        if letter.isalpha():
+            if letter.isupper():
+                index = alphabet.index(letter.lower())
+                # shift it 13 positions
+                translation = translation + alphabet[(index + 13) % 26].upper()
+            else:
+                index = alphabet.index(letter)
+                # shift it 13 positions
+                translation = translation + alphabet[(index + 13) % 26]
+        else:
+            translation = translation + letter
+
     return translation
 
 
 user_input = input("Enter a phrase: ")
-print(translate(user_input))
-print(translate(translate(user_input)))
+print("Translated: " + translate(user_input))
+print("Double Translated: " + translate(translate(user_input)))
