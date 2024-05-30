fcc2b067999c9dc7271462deb55a8a8192df1c2b
DSquire9 <dss9593@rit.edu>
2024-05-30 15:23:39 -0400

    Created a basic version of Conway's Game of Life

--- pythonProject/Board.py
+++ pythonProject/Board.py
@@ -9,13 +9,18 @@ class Board:
         self.size = size
         # note: [row][column]
         self.grid = []
-        for row in range(size):
+        self.previous = []
+        for row in range(self.size):
             r = []
-            for column in range(size):
+            for column in range(self.size):
+                # if random.randint(0, 1) == 0:
+                #     r.append(Cell(True, random.choice([1, 2, 3, 4])))
+                # else:
+                #     r.append(Cell(False, random.choice([1, 2, 3, 4])))
                 if random.randint(0, 1) == 0:
-                    r.append(Cell(True, random.choice([1, 2, 3, 4])))
+                    r.append(Cell(True, 1))
                 else:
-                    r.append(Cell(False, random.choice([1, 2, 3, 4])))
+                    r.append(Cell(False, 1))
             self.grid.append(r)
 
     def print_board(self):
@@ -27,3 +32,123 @@ class Board:
             result += "\n"
 
         print(result)
+
+    def progress(self):
+        self.previous = self.grid.copy()
+        new_grid = []
+        for row in range(self.size):
+            r = []
+            for column in range(self.size):
+                count = 0
+                if row == 0:
+                    if column == 0:
+                        if self.previous[row + 1][column].is_alive:
+                            count += 1
+                        if self.previous[row][column + 1].is_alive:
+                            count += 1
+                        if self.previous[row + 1][column + 1].is_alive:
+                            count += 1
+                    elif column == self.size - 1:
+                        if self.previous[row + 1][column].is_alive:
+                            count += 1
+                        if self.previous[row][column - 1].is_alive:
+                            count += 1
+                        if self.previous[row + 1][column - 1].is_alive:
+                            count += 1
+                    else:
+                        if self.previous[row + 1][column].is_alive:
+                            count += 1
+                        if self.previous[row][column + 1].is_alive:
+                            count += 1
+                        if self.previous[row + 1][column + 1].is_alive:
+                            count += 1
+                        if self.previous[row][column - 1].is_alive:
+                            count += 1
+                        if self.previous[row + 1][column - 1].is_alive:
+                            count += 1
+                elif row == self.size - 1:
+                    if column == 0:
+                        if self.previous[row - 1][column].is_alive:
+                            count += 1
+                        if self.previous[row - 1][column + 1].is_alive:
+                            count += 1
+                        if self.previous[row][column + 1].is_alive:
+                            count += 1
+                    elif column == self.size - 1:
+                        if self.previous[row - 1][column].is_alive:
+                            count += 1
+                        if self.previous[row - 1][column - 1].is_alive:
+                            count += 1
+                        if self.previous[row][column - 1].is_alive:
+                            count += 1
+                    else:
+                        if self.previous[row - 1][column].is_alive:
+                            count += 1
+                        if self.previous[row - 1][column + 1].is_alive:
+                            count += 1
+                        if self.previous[row][column + 1].is_alive:
+                            count += 1
+                        if self.previous[row - 1][column - 1].is_alive:
+                            count += 1
+                        if self.previous[row][column - 1].is_alive:
+                            count += 1
+                elif column == 0:
+                    if self.previous[row - 1][column].is_alive:
+                        count += 1
+                    if self.previous[row + 1][column].is_alive:
+                        count += 1
+                    if self.previous[row - 1][column + 1].is_alive:
+                        count += 1
+                    if self.previous[row + 1][column + 1].is_alive:
+                        count += 1
+                    if self.previous[row][column + 1].is_alive:
+                        count += 1
+                elif column == self.size - 1:
+                    if self.previous[row - 1][column].is_alive:
+                        count += 1
+                    if self.previous[row + 1][column].is_alive:
+                        count += 1
+                    if self.previous[row - 1][column - 1].is_alive:
+                        count += 1
+                    if self.previous[row + 1][column - 1].is_alive:
+                        count += 1
+                    if self.previous[row][column - 1].is_alive:
+                        count += 1
+                else:
+                    if self.previous[row - 1][column - 1].is_alive:
+                        count += 1
+                    if self.previous[row - 1][column].is_alive:
+                        count += 1
+                    if self.previous[row - 1][column + 1].is_alive:
+                        count += 1
+                    if self.previous[row][column - 1].is_alive:
+                        count += 1
+                    if self.previous[row][column + 1].is_alive:
+                        count += 1
+                    if self.previous[row + 1][column - 1].is_alive:
+                        count += 1
+                    if self.previous[row + 1][column].is_alive:
+                        count += 1
+                    if self.previous[row + 1][column + 1].is_alive:
+                        count += 1
+                if count < 2 and self.previous[row][column].is_alive:
+                    r.append(Cell(False, self.previous[row][column].faction))
+                elif count < 4 and self.previous[row][column].is_alive:
+                    r.append(Cell(True, self.previous[row][column].faction))
+                elif count > 3 and self.previous[row][column].is_alive:
+                    r.append(Cell(False, self.previous[row][column].faction))
+                elif count == 3 and not self.previous[row][column].is_alive:
+                    r.append(Cell(True, self.previous[row][column].faction))
+                else:
+                    r.append(Cell(False, self.previous[row][column].faction))
+            new_grid.append(r)
+        self.grid = new_grid
+        self.print_board()
+        # previous_board = ""
+        # for row in self.previous:
+        #     for column in row:
+        #         previous_board += column.to_str()
+        #
+        #     previous_board += "\n"
+        #
+        # print(previous_board)
--- pythonProject/Conways.py
+++ pythonProject/Conways.py
@@ -3,3 +3,5 @@ from Board import Board
 
 board = Board(10)
 board.print_board()
+for _ in range(50):
+    board.progress()
